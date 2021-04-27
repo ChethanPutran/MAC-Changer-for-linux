e7890e8a38103703eae362159d6e63c3192522fc
Chethan <chethansputran222@gmail.com>
2021-04-27 08:41:58 +0530

    all cleared

--- Macchanger/macchanger.py
+++ Macchanger/macchanger.py
@@ -4,12 +4,12 @@
 import subprocess
 import random
 import optparse
-import termcolor2
+import termcolor
 import colorama
 import re
 
 colorama.init()
-possibitities = ['A','B','C','D','E','F','1','2','3','4','5','6','7','8','9','0']
+possibitities = ['a','b','c','d','e','f','1','2','3','4','5','6','7','8','9','0']
  
 #Function to generate random MAC Address
 def getNewMAC():
@@ -19,22 +19,26 @@ def getNewMAC():
     #Creating random MAC Address
     for i in range(1,13):
         unit = random.choice(possibitities)
-        mac.append(unit)
+        if(i!=2):
+            mac.append(unit)
+        else:
+            unit = random.choice([ele for ele in possibitities if ele not in ['1','3','5','7','9','b','d','f']])
+            mac.append(unit)
         if ((i%2 == 0) and not(i==12)):
-            mac.append(':')
-    
+                mac.append(':')
+
     return ''.join(mac)   
 
 #Function to change MAC Address
 def changeMACAddress(interface,newMACAddress):
     
-    print(termcolor2.colored(("[+] *Changing MAC Address for "+ interface + " to new adress "+ newMACAddress ),'yellow'))
+    print(termcolor.colored(("[+] *Changing MAC Address for "+ interface + " to new adress "+ newMACAddress ),'yellow'))
     try:
-        subprocess.call(["ifconfig",interface," down"])
-        subprocess.call(["ifconfig",interface," hw"," ether ",newMACAddress])
-        subprocess.call(["ifconfig",interface," up",])
+        subprocess.call(["ifconfig",interface,"down"])
+        subprocess.call(["ifconfig",interface,"hw","ether",newMACAddress])
+        subprocess.call(["ifconfig",interface,"up",])
     except:
-        print(termcolor2.colored(("[!!] *Something went wrong! Quiting the progrm..."),'red'))
+        print(termcolor.colored(("[!!] *Something went wrong! Quiting the progrm..."),'red'))
         exit(0)
               
         
@@ -45,45 +49,53 @@ def getArguments():
     parser = optparse.OptionParser()
 
     parser.add_option("-i","--interface",dest="interface",help="Interface to change its MAC address")
-    parser.add_option("-m","--mac",dest="macAddress",help="New MAC address Eg. --resMAC FA:22:33:44:AD:22")
+    parser.add_option("-m","--mac",dest="macAddress",help="New MAC address Eg. --mac FA:22:33:44:AD:22")
     parser.add_option("-r","--random",dest="randomMAC",action="store_true", help="Assign random MAC Address")  
     
     (options,arguments) = parser.parse_args()
     
     #Checking if the user enter interface name or not
     if not options.interface:
-        print(termcolor2.colored(("[!!] *Interface name required! Use '--help' for more info."),'red'))
+        print(termcolor.colored(("[!!] *Interface name required! Use '--help' for more info."),'red'))
         exit(0)
-        
+    else:
+        try:
+            subprocess.check_output(["ifconfig",options.interface ])
+        except :
+            print(termcolor.colored(("[!!] *Interface not found!"),'red'))
+            exit(0)
+            
+            
     if((options.randomMAC == True ) and not(options.macAddress)):
         return (options.interface,None)
     elif(not(options.randomMAC) and (options.macAddress)):
         mac = options.macAddress
         test = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(mac)).group(0)
-   
+
         if(str(test)!=mac):
-            print(termcolor2.colored(("Invalid MAC Address!"),'red'))
+            print(termcolor.colored(("Invalid MAC Address!"),'red'))
             exit(0)
         else:
             for i in test:
                 if (i in possibitities) or (i==':'):
                     continue
                 else:
-                    print(termcolor2.colored(("Invalid MAC Address!"),'red'))
+                    print(termcolor.colored(("Invalid MAC Address!"),'red'))
                     exit(0)
             return (options.interface,options.macAddress)           
     else:
-        print(termcolor2.colored(("Can't use -m and -r together! Use '--help' for more info."), 'red'))
+        print(termcolor.colored(("Can't use -m and -r together! Use '--help' for more info."), 'red'))
         exit(0)
            
     
 
 def checkResult(interface,trueMAC):
-    results = subprocess.check_output(["ifconfig ",interface])
+    results = subprocess.check_output(["ifconfig",interface])
     resMAC = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(results))
-    if(resMAC):
-        if resMAC == trueMAC:
-            return True
+
+    if(resMAC.group(0) == trueMAC):
+        
+        return True
     else:
         return False        
     
@@ -93,16 +105,16 @@ interface,newMACAddress = getArguments()
 
 #Getting new MAC Address if user does not specify MAC Address
 if(not(newMACAddress)):
-    print(termcolor2.colored(("[+] *Configuring a random MAC Address to the interface..."),'yellow'))
+    print(termcolor.colored(("[+] *Configuring a random MAC Address to the interface..."),'yellow'))
     newMACAddress = getNewMAC()
-
+    
 
 changeMACAddress(interface,newMACAddress)
 status = checkResult(interface,newMACAddress)
 
 if(status):
-    print(termcolor2.colored(("[+] *MAC Address changed successfully..."),'green'))
+    print(termcolor.colored(("[+] *MAC Address changed successfully..."),'green'))
 else:
-    print(termcolor2.colored(("[+] *Unable to change MAC Address!!!"),'red'))
+    print(termcolor.colored(("[+] *Unable to change MAC Address!!!"),'red'))
     
 
